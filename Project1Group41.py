b60501260bedf613dda2be2101370963907048b3
MayaGuzunov <93738376+MayaGuzunov@users.noreply.github.com>
2021-11-04 21:27:06 +0100

    Initial commit

---
+++ Project1Group41.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 13:19:14 2021
+
+@author: mayaguzunov
+"""
+
+import pandas as pd
+import numpy as np
+
+filename=pd.read_csv('test.csv')
+print(filename)
+def dataLoad(filename):
+
+  return filename  
